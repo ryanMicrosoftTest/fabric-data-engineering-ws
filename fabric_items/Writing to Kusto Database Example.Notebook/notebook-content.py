3f0aef36e54c6bda74169662ffd1db0f1f80ecec
ryanMicrosoftTest <admin@mngenvmcap372892.onmicrosoft.com>
2026-04-03 12:44:54 +0000

    Committing 1 item from workspace a8cbda3d-903e-4154-97d9-9a91c95abb42

---
+++ fabric_items/Writing to Kusto Database Example.Notebook/notebook-content.py
@@ -0,0 +1,115 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# PARAMETERS CELL ********************
+
+# ingest parameters from pipeline
+parameter = "ImputVar_Two"
+kustoUri = "https://trd-e072h7tjkbkmz6nepn.z2.kusto.fabric.microsoft.com"
+database = "adf-eh-new"
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# # Writing to Kusto Databases Example
+
+# CELL ********************
+
+from pyspark.sql.types import StructType, StructField, StringType
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# get access token
+accessToken = mssparkutils.credentials.getToken(kustoUri)
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+import pandas as pd
+
+df = pd.DataFrame(
+    [(parameter)]
+)
+
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+schema = StructType([StructField('parameter', StringType(), True)])
+
+spark_df = spark.createDataFrame([(parameter,)], schema)
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+display(spark_df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# note that the only mode accepted by kusto writes is append mode
+spark_df.write\
+    .format("com.microsoft.kusto.spark.synapse.datasource")\
+    .option("accessToken", accessToken)\
+    .option("kustoCluster", kustoUri)\
+    .option("kustoDatabase", database)\
+    .option("kustoTable", "ppipelineParamsTbl")\
+    .mode('append')\
+    .save()
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
