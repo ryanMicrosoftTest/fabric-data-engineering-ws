0a0a23ace9d7792f8e30eeb16fb0df12f72184d3
root <root@DESKTOP-MS6O1GC.>
2026-04-08 12:43:44 -0400

    more updates

---
+++ scripts/sql/insert_into_claims_transactions_table.sql
@@ -0,0 +1,27 @@
+
+DECLARE @i INT = 1;
+WHILE @i <= 100
+BEGIN
+    INSERT INTO claims_transactions (id, claim_id, charge_id, patient_id, type, amount, method, from_date, to_date, last_updated_dttm)
+    VALUES (
+        CONCAT('TX', FORMAT(@i, '000')),
+        CONCAT('CLM', FORMAT(1000 + @i, '0000')),
+        CONCAT('CHG', FORMAT(2000 + @i, '0000')),
+        CONCAT('PAT', FORMAT(3000 + @i, '0000')),
+        CASE (@i % 3)
+            WHEN 0 THEN 'Consultation'
+            WHEN 1 THEN 'Surgery'
+            ELSE 'Lab Test'
+        END,
+        ROUND(RAND() * 1000 + 100, 2),
+        CASE (@i % 3)
+            WHEN 0 THEN 'Credit Card'
+            WHEN 1 THEN 'Insurance'
+            ELSE 'Cash'
+        END,
+        DATEADD(DAY, -(@i % 30), GETDATE()),
+        DATEADD(DAY, -(@i % 30 - 1), GETDATE()),
+        DATEADD(DAY, -1, GETDATE())
+    );
+    SET @i = @i + 1;
+END;
